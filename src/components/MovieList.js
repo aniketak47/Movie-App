287b2469145fe7951bede07de86707c98b5d7903
aniketak47 <aniketkr20012019@gmail.com>
2022-04-20 23:31:11 +0530

    first commit navbar banner movielist component

---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import { movies } from '../movieData'
+
+export class MovieList extends Component {
+  render() {
+    return (
+      <div>
+          
+      </div>
+    )
+  }
+}
+
+export default MovieList

74dba7437d09ad887bc2d756a176194b3278b984
aniketak47 <aniketkr20012019@gmail.com>
2022-04-27 22:22:17 +0530

    adding pagination

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -29,6 +29,18 @@ export class MovieList extends Component {
             ))
           }
         </div>
+
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+          </nav>
+        </div>
       </>
     )
   }

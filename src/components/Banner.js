641391cb755e0c3e7180a7216e3eb19183a3e150
aniketak47 <aniketkr20012019@gmail.com>
2022-04-27 21:05:18 +0530

    Movie List

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,8 +8,7 @@ export class Banner extends Component {
         
           <h5 className="card-title banner-title">Oblivion</h5>
           <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        
+                  
       </div>
     )
   }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,10 +4,27 @@ import { movies } from '../movieData'
 
 export class MovieList extends Component {
   render() {
+    let moviesArr = movies.results
+
     return (
-      <div>
-          
-      </div>
+      <>
+        <div>
+          <h3 className='text-center'><strong>Trending</strong></h3>
+        </div>
+
+        <div className="movies-list">
+          {
+            moviesArr.map((movieElem)=>(
+              <div className="card movie-card">
+                <img src="http://exclaim.ca/images/avatar_3.jpg" style={{height: '40vh', width: '20vw'}} className="card-img-top movie-img" alt="..."/>
+                
+                  <h5 className="card-title banner-title">{movieElem.title}</h5>                  
+                          
+              </div>
+            ))
+          }
+        </div>
+      </>
     )
   }
 }

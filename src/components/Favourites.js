64c3c2296314e3c8a1d56a12774ac0e71b60cddc
aniketak47 <aniketkr20012019@gmail.com>
2022-05-02 23:25:55 +0530

    list group updated

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -3,27 +3,50 @@ import React, { Component } from 'react'
 import { movies } from '../movieData'
 
 export class Favourites extends Component {
+
+    constructor(){
+        super()
+
+        this.state={
+            genres : []
+        }
+    }
+
   render() {
       const moviesArr = movies.results
 
       let genreids = {28:'Action',12:'Adventure',16:'Animation',35:'Comedy',80:'Crime',99:'Documentary',18:'Drama',10751:'Family',14:'Fantasy',36:'History',
        27:'Horror',10402:'Music',9648:'Mystery',10749:'Romance',878:'Sci-Fi',10770:'TV',53:'Thriller',10752:'War',37:'Western'};
 
+       let tempArr = []
+
+       moviesArr.map((movieObj)=>{
+           if(!tempArr.includes(genreids[movieObj.genre_ids[0]])){
+               tempArr.push(genreids[movieObj.genre_ids[0]])
+           }
+       })
+
+       tempArr.unshift('All Genres')
+
     return (
       <div className="main">
           <div className="row">
               <div className="col-3">
               <ul class="list-group  genre-selector">
-                <li class="list-group-item">Favourites</li>
-                <li class="list-group-item">Action</li>
-                <li class="list-group-item">Action</li>
-                <li class="list-group-item">Action</li>
-                <li class="list-group-item">Action</li>
+
+                    {
+                        tempArr.map((genres)=>(
+                            <li class="list-group-item">{genres}</li>
+                        ))
+                    }
+
+                
+                
                 </ul>
               </div>
               <div className="col-9 favourites-table">
                   <div className="row">
-                    <input type="text" className='input-group-text col' />
+                    <input placeholder='Search' type="text" className='input-group-text col' />
                     <input type="number" className='input-group-text col' />
                   </div>
 

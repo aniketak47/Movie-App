6c45e1e8d865e3d097d30c237f40f130acbc5ebd
aniketak47 <aniketkr20012019@gmail.com>
2022-05-11 22:12:04 +0530

    banner update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export class Banner extends Component {
         <img src={`https://image.tmdb.org/t/p/original${backDrop}`} className="card-img-top banner-img" alt="..."/>
         
           <h5 className="card-title banner-title">Oblivion</h5>
-          <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          {/* <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
                   
       </div>
     )
